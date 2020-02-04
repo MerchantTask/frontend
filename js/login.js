4fa2369a161e9728243bf65b3b65fa83cea69f9c
rajivphanju <razibphanju@gmail.com>
2020-02-04 16:00:05 +0545

    login fronted complete

---
+++ js/login.js
@@ -0,0 +1,42 @@
+
+    $(document).ready(function () {
+        var token = localStorage.getItem('token');
+     
+  
+        $("form.loginForm").on("submit",function(e){
+          e.preventDefault();
+          username = $("#username").val();
+          password = $("#password").val();
+  
+  
+          data = {
+            "username": username,
+            "password": password
+  
+          }
+          $.ajax({
+            url: 'http://localhost:8080/login/',
+            type: 'post',
+            dataType: 'json',
+            data: data,
+  
+            success: function (res, textStatus, xhr) {
+              if (res.token != null) {
+                localStorage.setItem('token', res.token);
+              
+               
+                  location.href = "dashboard.html";
+               
+  
+              } else {
+                alert(res.message);
+              }
+            },
+            
+            error: function (xhr, textStatus, errorThrown) {
+              console.log('Error in Operation');
+            }
+          });
+        });
+      });
+  
