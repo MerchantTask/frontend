47c7907f544388f95078116fc1fa63bacbbf810a
rajivphanju <razibphanju@gmail.com>
2020-02-05 15:57:05 +0545

    update merchant done

--- js/getallcompanies.js
+++ js/getallcompanies.js
@@ -20,7 +20,7 @@ $.getJSON('http://localhost:8000/Company/allCompanies/', function (res) {
       res[index].contact_email,
       res[index].company_email,
       res[index].pan,
-     '<a href="CompanyUpdate.html'+res[index]._id +'"><button class="btn"><i class="fa fa-cog fa-fw"></i></button></a>' +
+     '<a href="CompanyUpdate.html?id='+ res[index]._id +'"><button class="btn"><i class="fa fa-cog fa-fw"></i></button></a>' +
      '<button class="btn"><i class="fa fa-trash fa-fw" id="deleteCompany"></i></button>'
      
     ]).draw(false);
