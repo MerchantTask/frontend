bbe2bfcb71232b216241403302f844c0ff3d2263
rajivphanju <razibphanju@gmail.com>
2020-02-06 13:05:18 +0545

    image verification done

--- js/CompanyRegister.js
+++ js/CompanyRegister.js
@@ -1,4 +1,36 @@
 $(document).ready(function () {
+
+  let imageFile = '';
+  $("#verification_image").on('change', function () {
+    let formData = new FormData();
+    let files = $("#verification_image").get(0).files;
+    if (files.length > 0) {
+      formData.append("imageFile", files[0]);
+    }
+    $.ajax({
+      url: 'http://localhost:8000/Company/upload/',
+      type: 'post',
+      contentType: false,
+      cache: false,
+      processData: false,
+      data: formData,
+
+      success: function (data) {
+        imageFile = data.filename;
+
+        $('#image_display').html('<img src="http://localhost:8000/uploads/' + data.filename +
+          '" class="img-thumbnail" alt="Sample image" height="200px" width="200px">');
+
+      },
+      error: function () {
+        alert("Image upload failed");
+      }
+
+    });
+  });
+
+
+
 $("form.company").on("submit",function(e){
 
     e.preventDefault();
@@ -9,6 +41,7 @@ $("form.company").on("submit",function(e){
     contact_phone = $("#contact_phone").val();
     company_email = $("#email").val();
     pan = $("#pan").val();
+    listImage = imageFile;
  
 
     data ={
@@ -18,7 +51,8 @@ $("form.company").on("submit",function(e){
         "contact_email":contact_email,
         "contact_phone":contact_phone,
         "company_email":company_email,
-        "pan":pan
+        "pan":pan,
+        "verification_imagename": listImage
     }
     $.ajax({
         url:'http://localhost:8000/Company/addCompany/',
